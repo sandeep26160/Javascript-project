3eba3317b6304e940ddf3351cfbc940b0e8078e8
SANDEEP KUMAR YADAV <114286194+sandeep26160@users.noreply.github.com>
2024-09-16 00:21:28 +0000

    datatype and varibale

---
+++ basic/datatype.js
@@ -0,0 +1,29 @@
+"use strict";// treate all js code as new version
+
+// alert(3+3) //we are using  node js  not browser
+
+console.log(3+3)
+
+let name="namme"
+let age=18
+let isLogged= false
+
+
+//number=>2 to power52
+// bigint
+
+//sstring =>"",''
+//boolean=>true /false
+//null=>standalone value
+//undefined=>
+    //symbol=>unique
+
+//object
+console.log(typeof age);
+console.log(typeof null) ;//undefine
+
+// conversion of datatpe 
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
---
+++ basic/varabial.js
@@ -0,0 +1,8 @@
+const accountId=1455
+let accountEmail="sandeeepyadav@gmail.com"
+var accountPassword="123450"
+accountCitiy="ghazipur"
+
+// accountId =2
+
+console.log(accountId);

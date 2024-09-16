95bcf8cf2742ec056639cab37e3ff6a4fab010b3
SANDEEP KUMAR YADAV <114286194+sandeep26160@users.noreply.github.com>
2024-09-16 01:46:58 +0000

    memory

---
+++ basic/memory.js
@@ -0,0 +1,18 @@
+// stack and heap memory location
+
+let muYoutubename="Trainding song"
+let anotherName=muYoutubename
+anotherName="chai aur code"
+console.log(muYoutubename)
+console.log(anotherName)
+
+let user={
+    email:"user@google.com",
+    upi:"user@ybl"
+
+}
+let userTwo=user
+
+userTwo.email="asndeep@hmail.com"
+console.log(user);
+console.log(userTwo);
